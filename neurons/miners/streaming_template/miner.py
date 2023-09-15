0a08dad819b10c20f83a6ee71a3b15291cbcecc6
ifrit98 <ifrit98@gmail.com>
2023-09-15 21:02:05 +0000

    update streaming miner logic and docs

--- neurons/miners/streaming_template/miner.py
+++ neurons/miners/streaming_template/miner.py
@@ -31,6 +31,7 @@ mechanisms, custom configurations, and additional functionalities.
 import time
 import argparse
 from prompting.baseminer.miner import Miner
+from transformers import GPT2Tokenizer
 from prompting.protocol import StreamPrompting
 import bittensor as bt
 
@@ -65,45 +66,88 @@ class StreamingTemplateMiner(Miner):
 
     def prompt(self, synapse: StreamPrompting) -> StreamPrompting:
         """
-        Encapsulates the logic for generating a streaming response. It defines the tokenizer, model, and prompt functions,
-        and creates a streaming response using the provided synapse.
+        Generates a streaming response for the provided synapse.
+
+        This function serves as the main entry point for handling streaming prompts. It takes 
+        the incoming synapse which contains messages to be processed and returns a streaming 
+        response. The function uses the GPT-2 tokenizer and a simulated model to tokenize and decode 
+        the incoming message, and then sends the response back to the client token by token.
 
         Args:
-            synapse: A StreamPrompting instance containing the messages to be processed.
+            synapse (StreamPrompting): The incoming StreamPrompting instance containing the messages to be processed.
 
         Returns:
-            StreamPrompting: The streaming response object.
+            StreamPrompting: The streaming response object which can be used by other functions to 
+                            stream back the response to the client.
+
+        Usage:
+            This function can be extended and customized based on specific requirements of the 
+            miner. Developers can swap out the tokenizer, model, or adjust how streaming responses 
+            are generated to suit their specific applications.
         """
-        def tokenizer(text):
-            return (ord(char) for char in text)
+        tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
+        # Simulated function to decode token IDs into strings. In a real-world scenario, 
+        # this can be replaced with an actual model inference step.
         def model(ids):
-            return (chr(id) for id in ids)
+            return (tokenizer.decode(id) for id in ids)
 
-        async def prompt(text: str, send: Send):
+        async def _prompt(text: str, send: Send):
+            """
+            Asynchronously processes the input text and sends back tokens as a streaming response.
+
+            This function takes an input text, tokenizes it using the GPT-2 tokenizer, and then 
+            uses the simulated model to decode token IDs into strings. It then sends each token 
+            back to the client as a streaming response, with a delay between tokens to simulate 
+            the effect of real-time streaming.
+
+            Args:
+                text (str): The input text message to be processed.
+                send (Send): An asynchronous function that allows sending back the streaming response.
+
+            Usage:
+                This function can be adjusted based on the streaming requirements, speed of 
+                response, or the model being used. Developers can also introduce more sophisticated 
+                processing steps or modify how tokens are sent back to the client.
+            """
             # Simulate model inference
-            input_ids = tokenizer(text)
+            input_ids = tokenizer(text, return_tensors="pt").input_ids.squeeze()
+            # Iterate over the decoded tokens and send them back to the client.
             for token in model(input_ids):
+                # Send token back to the client
                 await send({"type": "http.response.body", "body": (token + '\n').encode('utf-8'), "more_body": True})
                 bt.logging.trace(f"Streamed token: {token}")
                 # Sleep to show the streaming effect
-                await asyncio.sleep(0.5)
+                await asyncio.sleep(1)
 
         message = synapse.messages[0]
-        token_streamer = partial(prompt, message)
+        # Bind the input message to the _prompt function and return the streaming response.
+        token_streamer = partial(_prompt, message)
         return synapse.create_streaming_response(token_streamer)
 
 
 # This is the main function, which runs the miner.
 if __name__ == "__main__":
     """
-    Main execution point for the TemplateMiner.
+    Entry point for executing the StreamingTemplateMiner.
 
-    This script initializes and runs the TemplateMiner, connecting it to the Bittensor network.
-    The miner listens for incoming requests and responds with a static message.
+    This block initializes the StreamingTemplateMiner and runs it, effectively connecting 
+    it to the Bittensor network. Once connected, the miner will continuously listen for 
+    incoming requests from the Bittensor network. For every request, it responds with a 
+    static message processed as per the logic defined in the 'prompt' method of the 
+    StreamingTemplateMiner class.
 
-    Developers can start the miner by simply executing this script. To add more functionalities
-    or customize the miner's behavior, consider extending the TemplateMiner class above.
+    The main loop at the end serves to keep the miner running indefinitely. It periodically 
+    prints a "running..." message to the console, providing a simple indication that the miner 
+    is operational and active.
+
+    Developers looking to extend or customize the miner's behavior can modify the 
+    StreamingTemplateMiner class and its methods. However, this block itself usually 
+    remains unchanged unless there's a need for specific startup behaviors or configurations.
+
+    To start the miner:
+    Simply execute this script. Ensure all dependencies are properly installed and network 
+    configurations are correctly set up.
     """
     with TemplateMiner():
         while True:
