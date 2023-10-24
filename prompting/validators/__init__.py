bd9ec3ad10298b0c86cbfd9d10bb55bbb711e331
p-ferreira <38992619+p-ferreira@users.noreply.github.com>
2023-10-24 14:42:06 -0400

    update versioning for release

--- prompting/validators/__init__.py
+++ prompting/validators/__init__.py
@@ -27,7 +27,7 @@ from . import weights
 from . import event
 from . import dataset
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 version_split = __version__.split(".")
 __spec_version__ = (
     (1000 * int(version_split[0]))
