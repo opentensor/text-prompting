27badfcdeea512efe37d3685b5a6173f082ee1eb
isabella618033 <isabella618033@gmail.com>
2023-11-08 22:58:09 +0000

    black formatting

--- prompting/validators/reward/blacklist.py
+++ prompting/validators/reward/blacklist.py
@@ -29,10 +29,12 @@ from dataclasses import dataclass
 
 # TODO: Use CLI arguments to set blacklist values: the most important being the boundary value and max_size
 
+
 @dataclass
 class BlacklistRewardEvent(BaseRewardEvent):
     matched_ngram: str = None
     significance_score: float = None
+
 
 class Blacklist(BaseRewardModel):
     @property
